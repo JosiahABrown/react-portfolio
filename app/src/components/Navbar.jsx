afeb6150a42b0106300d0b35e53a34c87b9153f3
Josiah Brown <88345601+JosiahABrown@users.noreply.github.com>
2023-07-01 16:58:30 -0500

    Basic Navbar outline

---
+++ app/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Navbar() {
+    return (
+        <nav className="fixed top-2 left-5 right-5 bg-l-brown rounded-full border-8 border-brown flex items-center justify-between p-4 md:px-8">
+            <p>Test</p>
+        </nav>
+    )
+}
