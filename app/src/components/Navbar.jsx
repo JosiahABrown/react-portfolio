f954fa2ace0ad373468b25cbbfd7e3e0a162786b
Josiah Brown <88345601+JosiahABrown@users.noreply.github.com>
2023-07-10 17:11:13 -0500

    Major Mono font added. Removed colors. Changed nav

--- app/src/components/Navbar.jsx
+++ app/src/components/Navbar.jsx
@@ -2,8 +2,9 @@ import React from "react";
 
 export default function Navbar() {
     return (
-        <nav className="fixed top-2 left-5 right-5 bg-l-brown rounded-full border-8 border-brown flex items-center justify-between p-4 md:px-8">
-            <p>Test</p>
+        <nav className="fixed top-2 left-5 right-5 flex items-center justify-between p-4 md:px-8">
+            <h1 className="flex-1 text-center font-major text-4xl text-gray-950 hover:text-red-700 
+                text-shadow-md-black">Josiah Brown</h1>
         </nav>
     )
 }
--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -3,12 +3,8 @@ export default {
   content: ["./src/**/*.{js,jsx}"],
   theme: {
     extend: {
-      colors: {
-        'white': "#f0ead2",
-        'l-green': "#dde5b6",
-        'green': "#adc178",
-        'l-brown': "#a98467",
-        'brown': "#6c584c",
+      fontFamily: {
+        major: ["MAJOR-MONO"] 
       },
     },
   },
