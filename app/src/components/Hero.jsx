19edc38901fd38cb4d6f8d42c61d441e822f4860
Josiah Brown <88345601+JosiahABrown@users.noreply.github.com>
2023-07-10 17:51:18 -0500

    Hero outline/box

---
+++ app/src/components/Hero.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Hero() {
+    return (
+        <div className="flex justify-center items-center w-screen h-screen">
+            <div className="bg-gray-200 border border-gray-950 w-11/12 h-4/6 md:w-9/12 md:h-5/6 overflow-y-auto"></div>
+        </div>
+    )
+}
