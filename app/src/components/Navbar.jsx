28f6758b6ef05ba6486aa14f3a73ad0212cb62f9
Josiah Brown <88345601+JosiahABrown@users.noreply.github.com>
2023-07-13 10:34:12 -0500

    Changed navbar into png logo

--- app/src/components/Navbar.jsx
+++ app/src/components/Navbar.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+// import logo from "../public/img/josiah-brown-navbar-logo.png";
 
 export default function Navbar() {
     return (
-        <nav className="fixed top-2 left-5 right-5 flex items-center justify-between p-4 md:px-8">
-            <h1 className="flex-1 text-center font-major text-4xl text-gray-950 hover:text-red-700 
-                text-shadow-md-black">Josiah Brown</h1>
+        <nav className="fixed flex justify-center p-4 md:px-8 w-screen h-20">
+            <div className="flex-1"></div>
+            <img className="flex-1 object-cover h-20" src="../img/josiah-brown-navbar-logo.png" alt="" />
+            <div className="flex-1"></div>
         </nav>
     )
 }
